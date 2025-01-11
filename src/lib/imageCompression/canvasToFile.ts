0ecb199e014a20ff55da8285b4f32d944d04bba6
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-01-11 15:38:45 +0800

    refactor compression code

---
+++ src/lib/imageCompression/canvasToFile.ts
@@ -0,0 +1,12 @@
+export function canvasToFile(canvas: HTMLCanvasElement): Promise<File> {
+  return new Promise((resolve) => {
+    canvas.toBlob(
+      (blob) => {
+        const cImage = new File([blob!], "compressed", { type: "image/jpg" });
+        resolve(cImage);
+      },
+      "image/jpeg",
+      0.7
+    );
+  });
+}
--- src/lib/imageCompression/imageCompression.ts
+++ src/lib/imageCompression/imageCompression.ts
@@ -1,30 +1,31 @@
-export async function CompressImage(file: File): Promise<File> {
-  try {
-    //create a img element with the file
-    const url = URL.createObjectURL(file);
-    const image = new Image();
-    image.src = url;
+import { canvasToFile } from "./canvasToFile";
+export function CompressImage(file: File): Promise<File> {
+  return new Promise((resolve, reject) => {
+    try {
+      //create a img element with the file
+      const url = URL.createObjectURL(file);
+      const image = new Image();
+      image.src = url;
 
-    //once image loads, use canvas to compress image
-    image.onload = async () => {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas?.getContext("2d");
+      //once image loads, use canvas to compress image
+      image.onload = async () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas?.getContext("2d");
 
-      //draw image on canvas
-      canvas!.height = image.height;
-      canvas!.width = image.width;
-      ctx?.drawImage(image, 0, 0, image.width, image.height);
+        //draw image on canvas
+        canvas!.height = image.height;
+        canvas!.width = image.width;
+        ctx?.drawImage(image, 0, 0, image.width, image.height);
 
-      //export canvas as compressed image
-      // const compressedImg = ctx?.canvas.toDataURL("image/jpeg", 0.7);
-      ctx?.canvas.toBlob((blob) => {}, "image/jpeg", 0.7);
-      // const res = await fetch(compressedImg!);
-      // const blob = await res.blob();
-      // const file = new File([blob], "compressed", { type: "image/jpeg" });
-      URL.revokeObjectURL(url);
-    };
-  } catch (error) {
-    if (error instanceof Error) throw new Error(error.message);
-    console.log(error);
-  }
+        //export canvas as compressed image
+        const cImage = await canvasToFile(ctx!.canvas!);
+        URL.revokeObjectURL(url);
+        resolve(cImage);
+      };
+    } catch (error) {
+      reject(error);
+      if (error instanceof Error) throw new Error(error.message);
+      console.log(error);
+    }
+  });
 }
