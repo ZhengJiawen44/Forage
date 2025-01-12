b642fd8a93edcaf647d7a1e927ae7f5322ea9f69
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-01-12 14:03:55 +0800

    dumped compressImage test due to JSdom issue

--- __tests__/compression.test.ts
+++ __tests__/compression.test.ts
@@ -1,27 +1,15 @@
-import { beforeEach, describe } from "node:test";
+/**
+ * @jest-environment jsdom
+ */
+import { describe } from "node:test";
 import { expect, test } from "@jest/globals";
-import fs from "node:fs";
 
-let image;
-describe("image compression", () => {
-  beforeEach(async () => {
-    image = await getFile();
-  });
+import { canvasToFile } from "@/lib/imageCompression/canvasToFile";
 
-  test("compression returns a File type object", async () => {
-    image = await getFile();
-    expect(image instanceof File).toBe(true);
+describe("canvasToFile", () => {
+  test("converts a canvas to a file", async () => {
+    const canvas = document.createElement("canvas");
+    const file = await canvasToFile(canvas);
+    expect(file instanceof File).toBe(true);
   });
 });
-
-async function getFile(): Promise<File> {
-  return new Promise((resolve, reject) => {
-    fs.readFile("__tests__/images/SantaClausVillage-UHD.jpg", (error, data) => {
-      if (error) {
-        reject(error);
-      }
-      const file = new File([data], "image", { type: "image/jpeg" });
-      resolve(file);
-    });
-  });
-}
