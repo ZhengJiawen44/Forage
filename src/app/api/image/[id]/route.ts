12b28b3ea55a4ec8e7c2bdb9b1b8d1017238bad1
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-01-20 01:25:18 +0800

    refactor api and pages

--- src/app/api/image/[id]/route.ts
+++ src/app/api/image/[id]/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 interface RouteParams {
   params: {
